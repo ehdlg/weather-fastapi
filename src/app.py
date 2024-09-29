e52d7f7b221914c21276f3e132b2a1d460f1039f
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-29 08:33:40 +0200

    feat: add get weahter endpoint, wip

--- src/app.py
+++ src/app.py
@@ -1,4 +1,9 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, Path, Query
+from requests import get
+
+from constants import API_KEY, API_URL
 
 app = FastAPI()
 
@@ -6,3 +11,25 @@ app = FastAPI()
 @app.get("/")
 def hello_world(name: str = "World"):
     return "Welcome to the Weather API made in FastAPI"
+
+
+@app.get("/{location}")
+def get_weather(
+    location: Annotated[
+        str,
+        Path(
+            description="Location to search the weather data",
+        ),
+    ],
+    country: Annotated[
+        str | None,
+        Query(description="Country to retrieve weather information."),
+    ] = None,
+):
+    if country is not None:
+        location = f"{location},{country}"
+
+    URL = f"{API_URL}/{location}?key={API_KEY}&unitGroup={'metric'}"
+    data = get(URL).json()
+
+    return data
