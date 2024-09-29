fb8e3671909cff570b78510d33c95958ae3a1ccf
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-29 09:30:22 +0200

    feat: add handle errors, implement generate_query func, use query as cache key

--- src/app.py
+++ src/app.py
@@ -1,17 +1,18 @@
 from json import dumps, loads
 from typing import Annotated
 
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI, HTTPException, Path, Query
 from requests import get
 
 from cache import get_cache, set_cache
 from constants import API_KEY, API_URL
+from utils import generate_query
 
 app = FastAPI()
 
 
 @app.get("/")
-def hello_world(name: str = "World"):
+def hello_api():
     return "Welcome to the WeatherAPI"
 
 
@@ -27,18 +28,34 @@ def get_weather(
         str | None,
         Query(description="Country to retrieve weather information."),
     ] = None,
+    language: Annotated[
+        str | None,
+        Query(
+            description="Sets the language of the translatable parts of the output such as the conditions field.",
+        ),
+    ] = None,
+    unit_group: Annotated[
+        str | None,
+        Query(
+            description="The system of units used for the output data. Supported values are us, uk, metric, base",
+        ),
+    ] = "metric",
 ):
-    if country is not None:
-        location = f"{location},{country}"
+    query = generate_query(location, country, language, unit_group)
 
-    URL = f"{API_URL}/{location}?key={API_KEY}&unitGroup={'metric'}"
+    URL = f"{API_URL}/{query}&key={API_KEY}"
 
-    cached_data = get_cache(URL)
+    cached_data = get_cache(query)
 
     if cached_data is not None:
         return loads(cached_data)
 
-    data = get(URL).json()
-    set_cache(URL, dumps(data))
+    response = get(URL)
+
+    if not response.ok:
+        raise HTTPException(status_code=response.status_code, detail=response.text)
+
+    data = response.json()
+    set_cache(query, dumps(data))
 
     return data
