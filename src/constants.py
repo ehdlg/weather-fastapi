fb8c62d0a00a584be5693fdbd3b5dae1ebaf232d
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-29 08:43:16 +0200

    feat: add redis connection variables from .env

--- src/constants.py
+++ src/constants.py
@@ -7,3 +7,6 @@ load_dotenv(env_path)
 
 API_URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline"
 API_KEY = getenv("API_KEY")
+REDIS_HOST = getenv("REDIS_HOST", "localhost")
+REDIS_PORT = getenv("REDIS_PORT", 6379)
+REDIS_PWD = getenv("REDIS_PWD", None)
