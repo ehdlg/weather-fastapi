ac15e3ccb865832947789289a876686d1a67d503
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-29 08:41:04 +0200

    feat: add API_KEY constant from .env

--- src/constants.py
+++ src/constants.py
@@ -1,1 +1,9 @@
+from os import getenv, path
+
+from dotenv import load_dotenv
+
+env_path = path.join(path.dirname(__file__), "..", ".env")
+load_dotenv(env_path)
+
 API_URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline"
+API_KEY = getenv("API_KEY")
